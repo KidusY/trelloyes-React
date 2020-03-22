76df305fe86dbcb64525117229c41541d7c678b4
DuyLuu90 <58050608+DuyLuu90@users.noreply.github.com>
2020-03-21 17:56:41 -0700

    update files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import List from './cardList/List';
+
+const STORE = {
+  lists: [
+    {
+      id: '1',
+      header: 'First list',
+      cardIds: [ 'a', 'b', 'e', 'f', 'g', 'j', 'l', 'm' ],
+    },
+    {
+      id: '2',
+      header: 'Second list',
+      cardIds: ['b', 'c', 'd', 'f', 'h', 'i', 'k'],
+    },
+    {
+      id: '3',
+      header: 'Third list',
+      cardIds: [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm' ],
+    },
+    {
+      id: '4',
+      header: 'Fourth list',
+      cardIds: [ 'l', 'm' ],
+    },
+  ],
+  allCards: {
+    'a': { id: 'a', title: 'First card', content: 'lorem ipsum' },
+    'b': { id: 'b', title: 'Second card', content: 'lorem ipsum' },
+    'c': { id: 'c', title: 'Third card', content: 'lorem ipsum' },
+    'd': { id: 'd', title: 'Fourth card', content: 'lorem ipsum' },
+    'e': { id: 'e', title: 'Fifth card', content: 'lorem ipsum' },
+    'f': { id: 'f', title: 'Sixth card', content: 'lorem ipsum' },
+    'g': { id: 'g', title: 'Seventh card', content: 'lorem ipsum' },
+    'h': { id: 'h', title: 'Eighth card', content: 'lorem ipsum' },
+    'i': { id: 'i', title: 'Ninth card', content: 'lorem ipsum' },
+    'j': { id: 'j', title: 'Tenth card', content: 'lorem ipsum' },
+    'k': { id: 'k', title: 'Eleventh card', content: 'lorem ipsum' },
+    'l': { id: 'l', title: 'Twelfth card', content: 'lorem ipsum' },
+    'm': { id: 'm', title: 'Thirteenth card', content: 'lorem ipsum' },
+  },
+}
+
 
 function App() {
+  const Lists=STORE.lists.map(obj=><List key={obj.id} header={obj.header} cards={obj.cardIds} allCards={STORE.allCards}/>);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="App"> 
+      <header>
+        <h1>Trelloyes!</h1>
       </header>
+      <div className='App-list'>
+        {Lists}
+      </div>
     </div>
   );
 }
